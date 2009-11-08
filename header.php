892788d6b5e4b5f21eb43ded5d2b8ff315b171a8
Dwi Asharialdy Hambali <dwiasharialdy@gmail.com>
2009-11-08 09:27:09 +0700

    page title modified

--- header.php
+++ header.php
@@ -10,7 +10,7 @@
   <head profile="http://gmpg.org/xfn/11">
   <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
 
-  <title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
+  <title><?php wp_title('&#8212;', true, 'right'); ?><?php bloginfo('name'); ?></title>
 
   <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
   <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
